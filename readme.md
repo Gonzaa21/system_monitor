12ee084ef2231aab76810c787dd3cb9889cef592
Gonzaa21 <gonzidibujante@gmail.com>
2025-05-13 16:56:46 -0300

    add: Inquire crate and create console UI

--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
-1. Separar las funciones en los otros archivos
-2. Crear una interfaz de consola
+1. ^^Separar las funciones en los otros archivos^^
+2. ^^Crear una interfaz de consola^^
 3. los datos se actualicen cada cierto tiempo
 4. Guardar los datos en tipo "snapshots" en formato .csv
 5. Sistema de alertas
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,49 @@
 use sysinfo::System;
 mod monitor;
+use colored::Colorize;
 
 fn main() {
+    select_system();
+}
+
+fn select_system() {
     let mut sys = System::new_all(); // Create instance
     sys.refresh_all(); // refreshing all
 
-    monitor::sys_information();
-    monitor::sys_components();
-    monitor::ram_memory(&sys);
-    monitor::swap_memory(&sys);
-    monitor::cpu(&sys);
-    monitor::processes(&sys);
-    monitor::disks();
-    monitor::networks();
+    // Inquire console
+    // Struct multiple options
+    let items = vec![
+        "System Information",
+        "Components",
+        "RAM Memory",
+        "SWAP Memory",
+        "CPU",
+        "Processes",
+        "Disk",
+        "Network"
+    ];
+
+    // messages
+    let msg = "Please select some of these options";
+    let expect = "Failed to select option".red().to_string();
+    
+    // create instance
+    let select = inquire::MultiSelect::new(msg, items)
+        .prompt()
+        .expect(expect.as_str());
+
+    // match options
+    for item in select {
+        match item {
+            "System Information" => monitor::sys_information(),
+            "Components" => monitor::sys_components(),
+            "RAM Memory" => monitor::ram_memory(&sys),
+            "SWAP Memory" => monitor::swap_memory(&sys),
+            "CPU" => monitor::cpu(&sys),
+            "Processes" => monitor::processes(&sys),
+            "Disk" => monitor::disks(),
+            "Network" => monitor::networks(),
+            _ => println!("Unknown option selected: {}", item)
+        }
+    }
 }
